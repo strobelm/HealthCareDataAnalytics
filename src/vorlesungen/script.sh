3d3e694a584c4823fb2f5dbdec1365a7006efc4b
Michael Strobel <m.strobel@posteo.de>
2022-03-07 21:39:52 +0100

    test pipe

--- src/vorlesungen/script.sh
+++ src/vorlesungen/script.sh
@@ -6,7 +6,7 @@ for d in */ ; do
     pdffile=${mdfile%.md}.pdf
     if [ $mdfile -nt $pdffile ]; then
       echo $mdfile
-      pandoc -t beamer --include-in-header ../header.tex $mdfile -o $pdffile
+      pandoc -t beamer --include-in-header ../header.tex $mdfile -o ../../../build/$pdffile
       echo "Done"
     fi
   done
